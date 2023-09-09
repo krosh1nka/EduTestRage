fae7ec2cabd7fc8f4803aad7a2b4c008a29efe9f
Andrii Stiebalov <stiebalov@gmail.com>
2023-09-09 13:19:17 +0300

    puhs

--- vue.config.js
+++ vue.config.js
@@ -2,12 +2,28 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: '',
-  filenameHashing: false,
   productionSourceMap: false,
-  outputDir: '../client_packages/spa',
-  // configureWebpack: {
-  //   devServer: {
-  //     overlay: false
-  //   }
-  // }
+  filenameHashing: true,
+  lintOnSave: true,
+  runtimeCompiler: true,
+  css: {
+    extract: false,
+    sourceMap: false
+  },
+  configureWebpack: {
+    cache: {
+      type: 'filesystem', // Используйте тип 'filesystem' для инкрементальной пересборки
+      buildDependencies: {
+        config: [__filename] // Используйте эту строку, чтобы при изменении конфигурации Webpack также пересобирать проект
+      }
+    },
+    optimization: {
+      splitChunks: false
+    }
+  },
+  chainWebpack: (config) => {
+    config.module
+      .rule('images')
+      .set('parser', { dataUrlCondition: { maxSize: 0 } })
+  }
 })
